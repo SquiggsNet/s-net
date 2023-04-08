d489608029a9a937f0cd303aa7f8ada3aa41618b
Scott <squiggs.rafael@gmail.com>
2023-04-07 22:25:56 -0300

    working posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,11 +9,23 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const {user} = useUser();
+  const [input, setInput] = useState("");
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.invalidate();
+    }
+  });
+
   if (!user) return null; 
 
   return (
@@ -25,7 +37,15 @@ const CreatePostWizard = () => {
         width={56}
         height={56}
       />
-      <input placeholder="Type some emojis!" className="bg-transparent grow outline-none"/>
+      <input
+        placeholder="Type some emojis!"
+        className="bg-transparent grow outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
+      />
+      <button onClick={() => mutate({ content: input})}>Post</button>
     </div>
   );
 }
